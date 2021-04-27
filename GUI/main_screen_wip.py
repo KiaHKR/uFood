37771dd199c25af816e9e3266514db47123ee9e1
Lasse Poulsen <work.lasse.face@gmail.com>
2021-04-27 12:35:15 +0200

    inserted inits for each folder

--- GUI/main_screen_wip.py
+++ GUI/main_screen_wip.py
@@ -24,8 +24,8 @@ class MainWindow(qtw.QWidget):
 
         """
         super().__init__(*args, **kwargs)
-        self.resize(int(wsize*0.5), int(hsize*0.5))
-        self.setStyleSheet('background-color: #1c1c1c;')
+        self.resize(int(wsize * 0.5), int(hsize * 0.5))
+        self.setStyleSheet("background-color: #1c1c1c;")
 
         lpanel = self.set_left_panel()
         rt_panel = self.set_right_tpanel()
@@ -40,17 +40,21 @@ class MainWindow(qtw.QWidget):
         scroll_area = qtw.QScrollArea()
         scroll_area.setWidget(rb_panel)
         scroll_area.setWidgetResizable(True)
-        scroll_area.verticalScrollBar().setStyleSheet("""
+        scroll_area.verticalScrollBar().setStyleSheet(
+            """
             background-color: powderblue;
             color: black;
-        """)
-        scroll_area.horizontalScrollBar().setStyleSheet("""
+        """
+        )
+        scroll_area.horizontalScrollBar().setStyleSheet(
+            """
             background-color: powderblue;
             color: black;
-        """)
+        """
+        )
 
         rpanel = qtw.QWidget()
-        rpanel.setStyleSheet('border: 2px solid green;')
+        rpanel.setStyleSheet("border: 2px solid green;")
         rpanel.setLayout(qtw.QVBoxLayout())
         rpanel.layout().addWidget(rt_panel, 0)
         rpanel.layout().addWidget(scroll_area, 7)
@@ -66,8 +70,8 @@ class MainWindow(qtw.QWidget):
     def set_left_panel(self):
         """Build the left vbox panel."""
         logo = qtw.QLabel(
-            pixmap=qtg.QPixmap('assets/logo_placeholder.png').scaled(200, 200)
-            )
+            pixmap=qtg.QPixmap("assets/logo_placeholder.png").scaled(200, 200)
+        )
         # logo.setStyleSheet('border: 2px solid red;')
         logo.setAlignment(qtc.Qt.AlignCenter)
 
@@ -78,15 +82,15 @@ class MainWindow(qtw.QWidget):
 
         menu = qtw.QMenu()
         menu.setContentsMargins(5, 0, 0, 0)
-        act1 = qtw.QAction('Vegan', menu)
+        act1 = qtw.QAction("Vegan", menu)
         act1.setCheckable(True)
-        act2 = qtw.QAction('Vegetarian', menu)
+        act2 = qtw.QAction("Vegetarian", menu)
         act2.setCheckable(True)
-        act3 = qtw.QAction('Pescatarian', menu)
+        act3 = qtw.QAction("Pescatarian", menu)
         act3.setCheckable(True)
-        act4 = qtw.QAction('Keto', menu)
+        act4 = qtw.QAction("Keto", menu)
         act4.setCheckable(True)
-        act5 = qtw.QAction('Paleo', menu)
+        act5 = qtw.QAction("Paleo", menu)
         act5.setCheckable(True)
 
         menu.addAction(act1)
@@ -97,30 +101,34 @@ class MainWindow(qtw.QWidget):
 
         dietary_filter = qtw.QPushButton()
         dietary_filter.setMenu(menu)
-        dietary_filter.setStyleSheet("""
+        dietary_filter.setStyleSheet(
+            """
             margin-left: 5px;
             background-color: white;
-            """)
+            """
+        )
         dietary_filter.setFixedWidth(25)
 
         pre = qtw.QLabel()
         pre.setLayout(qtw.QStackedLayout())
 
         search_bar = qtw.QLineEdit()
-        search_bar.setStyleSheet('background-color: white; font-size: 14px;')
+        search_bar.setStyleSheet("background-color: white; font-size: 14px;")
         search_bar.setFixedSize(240, 40)
         search_bar.setContentsMargins(20, 0, 20, 0)
         search_button = qtw.QPushButton(
-            icon=qtg.QIcon(qtg.QPixmap('assets/search.png'))
-            )
+            icon=qtg.QIcon(qtg.QPixmap("assets/search.png"))
+        )
         search_button.setIconSize(qtc.QSize(25, 25))
-        search_button.setStyleSheet("""
+        search_button.setStyleSheet(
+            """
             background-color: transparent;
             margin-top: 1px;
             position: absolute;
-        """)
-        pre.setStyleSheet('background-color: white; margin-top: 1.1px;')
-        pre.setFixedSize(qtc.QSize(30, search_bar.height()-1))
+        """
+        )
+        pre.setStyleSheet("background-color: white; margin-top: 1.1px;")
+        pre.setFixedSize(qtc.QSize(30, search_bar.height() - 1))
         search_button.setFixedSize(qtc.QSize(30, search_bar.height()))
 
         search = qtw.QWidget()
@@ -133,13 +141,13 @@ class MainWindow(qtw.QWidget):
         search.layout().setAlignment(qtc.Qt.AlignTop)
         search.layout().setSpacing(0)
 
-        donate_text = qtw.QLabel(text='DONATE')
-        donate_text.setStyleSheet('color: white; font-weight: bold;')
+        donate_text = qtw.QLabel(text="DONATE")
+        donate_text.setStyleSheet("color: white; font-weight: bold;")
         fb_icon = qtw.QPushButton(
-            icon=qtg.QIcon(qtg.QPixmap('assets/fb_icon.png'))
-            )
+            icon=qtg.QIcon(qtg.QPixmap("assets/fb_icon.png"))
+        )
         fb_icon.setIconSize(qtc.QSize(30, 30))
-        fb_icon.setStyleSheet('background: transparent;')
+        fb_icon.setStyleSheet("background: transparent;")
         donate = qtw.QWidget()
         # donate.setStyleSheet('border: 2px solid red')
         donate.setLayout(qtw.QHBoxLayout())
@@ -161,31 +169,31 @@ class MainWindow(qtw.QWidget):
     def set_right_tpanel(self):
         """Set top rightmost hbox panel [icon bar]."""
         r_top = qtw.QWidget()
-        r_top.setStyleSheet('border:2px solid powderblue;')
+        r_top.setStyleSheet("border:2px solid powderblue;")
         r_top.setLayout(qtw.QHBoxLayout())
 
         text = qtw.QLabel()
-        text.setStyleSheet('border: none; color: white; font-size: 20px')
+        text.setStyleSheet("border: none; color: white; font-size: 20px")
         favorites_icon = qtw.QPushButton(
-            icon=qtg.QIcon(qtg.QPixmap('assets/fav_icon.png'))
-            )
-        favorites_icon.setStyleSheet('border: None; margin: auto;')
+            icon=qtg.QIcon(qtg.QPixmap("assets/fav_icon.png"))
+        )
+        favorites_icon.setStyleSheet("border: None; margin: auto;")
         # favorites_icon.setStyleSheet('background-color: white;')
         favorites_icon.setFixedSize(40, 30)
         favorites_icon.setIconSize(qtc.QSize(30, 30))
 
         recipes_icon = qtw.QPushButton(
-            icon=qtg.QIcon(qtg.QPixmap('assets/recipes_icon.png'))
-            )
-        recipes_icon.setStyleSheet('border: None; margin-top: 4px;')
+            icon=qtg.QIcon(qtg.QPixmap("assets/recipes_icon.png"))
+        )
+        recipes_icon.setStyleSheet("border: None; margin-top: 4px;")
         # favorites_icon.setStyleSheet('background-color: white;')
         recipes_icon.setFixedSize(40, 30)
         recipes_icon.setIconSize(qtc.QSize(30, 30))
 
         back_icon = qtw.QPushButton(
-            icon=qtg.QIcon(qtg.QPixmap('assets/back_icon.png'))
-            )
-        back_icon.setStyleSheet('border: None;')
+            icon=qtg.QIcon(qtg.QPixmap("assets/back_icon.png"))
+        )
+        back_icon.setStyleSheet("border: None;")
         # favorites_icon.setStyleSheet('background-color: white;')
         back_icon.setFixedSize(40, 30)
         back_icon.setIconSize(qtc.QSize(30, 30))
@@ -198,24 +206,24 @@ class MainWindow(qtw.QWidget):
 
     # responsive slot method that governs what pages load from the
     # ... rbp.RightBottomPanel() class?
-    def set_right_bpanel(self, page='trending'):
+    def set_right_bpanel(self, page="trending"):
         """Set right bottom panel/widget w.r.t. emitted signals/events."""
         rp_bottom = None
-        if page.lower() == 'trending':
+        if page.lower() == "trending":
             rp_bottom = rbp.RightBottomPanel().trending_page()
-        elif page.lower() == 'search_results':
+        elif page.lower() == "search_results":
             rp_bottom = rbp.RightBottomPanel().search_results_page()
-        elif page.lower() == 'favorites':
+        elif page.lower() == "favorites":
             rp_bottom = rbp.RightBottomPanel().favorite_page()
         return rp_bottom
 
 
-if __name__ == '__main__':
-    product_name = 'uFood'
+if __name__ == "__main__":
+    product_name = "uFood"
     app = qtw.QApplication(sys.argv)
-    win_icon = qtg.QIcon(qtg.QPixmap('assets/carrot_icon.png'))
+    win_icon = qtg.QIcon(qtg.QPixmap("assets/carrot_icon.png"))
     screen = app.primaryScreen().size()
     hsize = screen.height()  # kept seperate rather than list
-    wsize = screen.width()   # ... so that it is more pythonic
+    wsize = screen.width()  # ... so that it is more pythonic
     mw = MainWindow(windowTitle=product_name, windowIcon=win_icon)
     sys.exit(app.exec_())
