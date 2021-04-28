da894f80a1c0c00bd0db32072b414c4e9cd2c6ed
RammeDon <raminkhareji@gmail.com>
2021-04-28 15:20:43 +0200

    broke down the code and updated the timer

--- GUI/rb_panel.py
+++ GUI/rb_panel.py
@@ -15,42 +15,78 @@ class RightBottomPanel():
 
     def trending_page(self):
         """Build trending page."""
-
-        for _ in range(0, 20):
-            rp_bottom = qtw.QWidget()
-            rp_bottom.setStyleSheet(
-                    """border: 2px solid purple;
-                    color: white;
-                    font-size: 20px;"""
-                )
-            rp_bottom.setLayout(qtw.QVBoxLayout())
-            rp_bottom.layout().addWidget(qtw.QLabel(text="this panel will change")) 
-
-        return rp_bottom
+        pass
 
     def search_results_page(self):
         """Build search results page."""
 
         option = qtw.QWidget()
 
-        # creating left and right Widgets
-        pic_widget = qtw.QWidget()
-        pic_widget.setLayout(qtw.QVBoxLayout())
+        self.create_recipe_label()
+
+        self.create_diet_type_label()
+        
+        self.create_cooking_time()
+
+        self.create_cooking_time_pic()
+
+        self.create_ingredients_list()
+
+        self.create_cooking_timer()
+
+        self.cooking_timer.layout().addWidget(self.cooking_time_pic)
+        self.cooking_timer.layout().addWidget(self.cooking_time)
+
+        self.create_ingredient_label()
+
+        self.ingredients_label.layout().addWidget(self.cooking_timer)
+        self.ingredients_label.layout().addWidget(self.ingredients_list)
+        self.ingredients_label.layout().setContentsMargins(0, 0, 0, 0)
+
+        self.create_label_widget()
+        self.label_widget.layout().addWidget(self.recipe_label)
+        self.label_widget.layout().addWidget(self.diet_type_label)
+        self.label_widget.layout().addWidget(self.ingredients_label)
+
+        # pic_widget and label_widget get added to parent widget
+        option.setLayout(qtw.QHBoxLayout())
+        self.create_left_parent()
+        option.layout().addWidget(self.pic_widget)
+        option.layout().addWidget(self.label_widget)
+
+        return option
+
+    def favorite_page(self):
+        """Build favorite page."""
+        pass
+
+    def create_left_parent(self):
+        self.pic_widget = qtw.QWidget()
+        self.pic_widget.setLayout(qtw.QVBoxLayout())
+        self.pic_widget.layout().setContentsMargins(0, 0, 0, 0)
 
         picture = qtw.QLabel(
             pixmap=qtg.QPixmap("assets/test_asser.png").scaled(120, 120)
             )
         picture.setFixedWidth(120)
-        picture.setFixedHeight(120)  
+        picture.setFixedHeight(120)
 
-        pic_widget.layout().addWidget(picture) 
-        pic_widget.setFixedWidth(125)
+        self.pic_widget.layout().addWidget(picture)
+        self.pic_widget.setFixedWidth(120)
 
-        pic_widget.layout().setAlignment(qtc.Qt.AlignTop)  
-        pic_widget.layout().setSpacing(0)  
+        self.pic_widget.layout().setAlignment(qtc.Qt.AlignTop)
 
-        # unecessary bit added for showcase
+    def create_label_widget(self):
+        self.label_widget = qtw.QWidget()
+        self.label_widget.setLayout(qtw.QVBoxLayout())
+        self.label_widget.layout().setContentsMargins(0, 0, 0, 0)
 
+    def create_recipe_label(self):
+        self.recipe_label = qtw.QLabel("[FOOD NAME]")
+        self.recipe_label.setFixedHeight(30)
+        self.recipe_label.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
+
+    def create_diet_type_label(self):
         food_type_list = ["Vegan", "Vegetarian", "Pescatarian", "keto"]
         food_type = food_type_list[randrange(4)]
         if food_type == "Vegan":
@@ -61,57 +97,32 @@ class RightBottomPanel():
             diet_colour = "#2a75a3"
         elif food_type == "keto":
             diet_colour = "#d18436"
+        self.diet_type_label = qtw.QLabel(f"{food_type}")
+        self.diet_type_label.setFixedHeight(20)
+        self.diet_type_label.setStyleSheet(f"color: {diet_colour}; font-size: 16px; font-weight: bold;")
 
-        # Left widget
-        label_widget = qtw.QWidget()
-        label_widget.setLayout(qtw.QVBoxLayout())
+    def create_ingredient_label(self):
+        self.ingredients_label = qtw.QWidget()
+        self.ingredients_label.setLayout(qtw.QVBoxLayout())
 
-        recipe_label = qtw.QLabel("[FOOD NAME]")
-        recipe_label.setStyleSheet("color: white; font-size: 20px; font-weight: bold;")
-        diet_type_label = qtw.QLabel(f"{food_type}")
-        diet_type_label.setStyleSheet(f"color: {diet_colour}; font-size: 16px; font-weight: bold;")
-        
-        
-        ingredients_label = qtw.QWidget()
-        ingredients_label.setLayout(qtw.QVBoxLayout())
+    def create_cooking_timer(self):
+        self.cooking_timer = qtw.QWidget()
+        self.cooking_timer.setLayout(qtw.QHBoxLayout())
+        self.cooking_timer.layout().setSpacing(0)
+        self.cooking_timer.layout().setAlignment(qtc.Qt.AlignLeft)
+        self.cooking_timer.layout().setContentsMargins(0, 0, 0, 0)
 
-
-        cooking_timer = qtw.QWidget()
-        cooking_timer.setLayout(qtw.QHBoxLayout())
-
-        cooking_time_pic = qtw.QLabel(
-            pixmap=qtg.QPixmap("assets/timer.png").scaled(15, 15)
+    def create_cooking_time_pic(self):
+        self.cooking_time_pic = qtw.QLabel(
+            pixmap=qtg.QPixmap("assets/timer2.png").scaled(20, 20)
             )
-        cooking_time_pic.setFixedWidth(20)
+        self.cooking_time_pic.setFixedWidth(25)
 
-        cooking_time = qtw.QLabel("[COOKING TIME]")
-        cooking_time.setFixedHeight(20)
-        cooking_time.setStyleSheet("color: white; font-size: 8px; font-weight: bold;")
+    def create_cooking_time(self):
+        self.cooking_time = qtw.QLabel("[COOKING TIME]")
+        self.cooking_time.setFixedHeight(20)
+        self.cooking_time.setStyleSheet("color: white; font-size: 8px; font-weight: bold;")
 
-        ingredients_list = qtw.QLabel("INGREDIENT LIST")
-        ingredients_list.setStyleSheet("color: white; font-size: 10px; font-weight: bold;")
-
-        cooking_timer.layout().addWidget(cooking_time_pic)
-        cooking_timer.layout().addWidget(cooking_time)
-        cooking_timer.layout().setSpacing(0)
-        cooking_timer.layout().setAlignment(qtc.Qt.AlignLeft)
-        cooking_timer.layout().setContentsMargins(0, 0, 0, 0)
-
-        ingredients_label.layout().addWidget(cooking_timer)
-        ingredients_label.layout().addWidget(ingredients_list)
-        ingredients_label.setFixedHeight(100)
-
-        label_widget.layout().addWidget(recipe_label)
-        label_widget.layout().addWidget(diet_type_label)
-        label_widget.layout().addWidget(ingredients_label)
-
-        # this is where the two main Widgets get added
-        option.setLayout(qtw.QHBoxLayout())
-        option.layout().addWidget(pic_widget)
-        option.layout().addWidget(label_widget)
-
-        return option
-
-    def favorite_page(self):
-        """Build favorite page."""
-        pass
+    def create_ingredients_list(self):
+        self.ingredients_list = qtw.QLabel("INGREDIENT LIST")
+        self.ingredients_list.setStyleSheet("color: white; font-size: 12px; font-weight: bold;")
