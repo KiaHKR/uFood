781d76062e7596c428e4d29a78400c51b4ef4ad6
Tom <welter.tom@hotmail.com>
2021-04-29 22:07:39 +0200

    Finalized first query, deleted unnecessary files.

--- src/search.py
+++ src/search.py
@@ -3,6 +3,7 @@ import mysql.connector
 
 class Search:
 
+
     mydb = None
     db_ingredients = [
         "ass",
@@ -16,12 +17,19 @@ class Search:
     ]
 
     def __init__(self):
-        # self.mydb = mysql.connector.connect(
-        #     host="localhost", user="root", password="Regnskov123!"
-        # )
+        self.mydb = mysql.connector.connect(
+            host = "localhost",
+            user = "dbread",
+            password = "6stooges",
+            database = "ufood"
+        ) 
+        self.mycursor = self.mydb.cursor()
         pass
 
-    def recipe_name_search(self):
+    def recipe_name_search(self, srch):
+        self.mycursor.execute("SELECT recipes.name as 'Recipe name', recipes.cooking_time as 'Cooking time',GROUP_CONCAT(DISTINCT recipes_has_diets.diets_name) as 'Diets',GROUP_CONCAT(ingredients_has_recipes.ingredients_name) as 'Ingredients' FROM recipes INNER JOIN recipes_has_diets ON recipes.id = recipes_has_diets.recipes_id INNER JOIN ingredients_has_recipes ON recipes.id = ingredients_has_recipes.recipes_id WHERE recipes.name LIKE '%%"+srch+"%%' Group by name")
+        for x in self.mycursor:
+            print(x)
         pass
 
     def ingredient_name_search(self):
