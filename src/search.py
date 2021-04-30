b00f3240aa5e91ad5520bc704ddb5551a0e43fdd
Tom <welter.tom@hotmail.com>
2021-04-30 18:14:17 +0200

    Updated query.

--- src/search.py
+++ src/search.py
@@ -27,13 +27,26 @@ class Search:
         pass
 
     def recipe_name_search(self, srch):
+        finallist1 = []
         self.mycursor.execute("SELECT recipes.name as 'Recipe name', recipes.cooking_time as 'Cooking time',GROUP_CONCAT(DISTINCT recipes_has_diets.diets_name) as 'Diets',GROUP_CONCAT(ingredients_has_recipes.ingredients_name) as 'Ingredients' FROM recipes INNER JOIN recipes_has_diets ON recipes.id = recipes_has_diets.recipes_id INNER JOIN ingredients_has_recipes ON recipes.id = ingredients_has_recipes.recipes_id WHERE recipes.name LIKE '%%"+srch+"%%' Group by name")
         for x in self.mycursor:
-            print(x)
-        pass
+            finallist1.append(x)
+        return finallist1
 
-    def ingredient_name_search(self):
-        pass
+    def ingredient_name_search(self, ing_list):
+        ingred = ""
+        finallist = []
+        for x in ing_list[0:len(ing_list)-1]:
+            ingred += "ingredients_has_recipes.ingredients_name = '" + x + "' or "
+        ingred += " ingredients_has_recipes.ingredients_name = '" + ing_list[-1] + "'"    
+        self.mycursor.execute("SELECT recipes.name as 'Recipe name', recipes.cooking_time as 'Cooking time', GROUP_CONCAT(DISTINCT recipes_has_diets.diets_name) as 'Diets', GROUP_CONCAT(ingredients_has_recipes.ingredients_name) as 'Ingredients' FROM recipes INNER JOIN recipes_has_diets ON recipes.id = recipes_has_diets.recipes_id INNER JOIN ingredients_has_recipes ON recipes.id = ingredients_has_recipes.recipes_id WHERE "+ ingred +" GROUP BY name")
+        for x in self.mycursor:
+            finallist.append(x)
+        return finallist
 
-    def cooking_time_search(self):
-        pass
+    def cooking_time_search(self, time):
+        finallist1 = []
+        self.mycursor.execute("SELECT recipes.name as 'Recipe name', GROUP_CONCAT(ingredients_has_recipes.ingredients_name) as 'Ingredients', GROUP_CONCAT(DISTINCT recipes_has_diets.diets_name) as 'Diets', recipes.cooking_time as 'Cooking time' FROM recipes INNER JOIN recipes_has_diets ON recipes.id = recipes_has_diets.recipes_id INNER JOIN ingredients_has_recipes ON recipes.id = ingredients_has_recipes.recipes_id WHERE cooking_time < '"+ time + "'  GROUP BY name;")
+        for x in self.mycursor:
+            finallist1.append(x)
+        return finallist1
