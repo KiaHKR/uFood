b54dada99a4c6cf817d26637de819b353dc174a6
Lasse Poulsen <work.lasse.face@gmail.com>
2021-05-05 15:42:12 +0200

    fixed alignment issue

--- src/interface/root.py
+++ src/interface/root.py
@@ -175,6 +175,7 @@ class View(qtw.QWidget):
         bottom_layout = qtw.QWidget()
         # bottom_layout.setStyleSheet("border:2px solid green;")
         bottom_layout.setLayout(qtw.QVBoxLayout())
+        bottom_layout.layout().setAlignment(qtc.Qt.AlignmentFlag.AlignTop)
 
         # while there are results
         bottom_layout.layout().addWidget(self.__recipe_card())
