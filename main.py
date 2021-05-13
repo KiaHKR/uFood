53de0df1abf6085a6e70e3512822ab8e7e743ddb
sam-hur <83226062+sam-hur@users.noreply.github.com>
2021-05-13 12:40:08 +0200

    Added a system check to make sure that the program only runs on Windows computers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """Main file for launching application."""
 from src.interface.root import View
-
+import src.bin.validation as validation
 
 if __name__ == "__main__":
+    validation  # run a system validation script
     v = View()
---
+++ src/bin/validation.py
@@ -0,0 +1,20 @@
+"""Runs a script to check that all validations are correct."""
+import platform
+
+
+suppOS = ["Windows"]
+val = True
+
+
+def checkOS():
+    """Check that OS is Supported."""
+    os = platform.system()
+    if os not in suppOS:
+        msg1 = "Your operating system ({}) is not supported in this build. "
+        msg2 = "Compatible operating system(s): {}"
+        x = ", ".join(suppOS)
+
+        raise SystemExit(msg1.format(os) + msg2.format(x))
+
+
+checkOS()
