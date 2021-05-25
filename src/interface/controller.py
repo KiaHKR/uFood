1b788b6afed48a6015ae8a61c340549e865de9ed
Lasse Poulsen <work.lasse.face@gmail.com>
2021-05-25 12:39:14 +0200

    bug: img loading took long. Removed img loading for now

--- src/interface/controller.py
+++ src/interface/controller.py
@@ -298,7 +298,7 @@ class Controller:
                     str(i[1])[:-3],  # COOKTIME
                     i[3].replace(",", ", "),
                     str(i[4]),  # ID
-                    i[5],  # URL
+                    None,  # URL
                 )
 
             temp_widget_list.append(recipe_card)
